b71b5ad2db819cf71bd123fb301d5aef8e823881
guy djoumessi <gmdnegocio@gmail.com>
2025-03-04 23:46:45 -0300

    Initial commit

---
+++ src/main/java/com/macota/MacootapiApplication.java
@@ -0,0 +1,13 @@
+package com.macota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacootapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacootapiApplication.class, args);
+	}
+
+}
